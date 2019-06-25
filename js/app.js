2201b50224990cba8224c2769d19595fe9a94105
GA Denver <gadenver@Saras-MacBook-Air.local>
2019-06-25 10:35:58 -0600

    bondFilms gross

--- js/app.js
+++ js/app.js
@@ -166,5 +166,101 @@ console.log(inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dr
 
 
 
+const bondFilms = [
+  { "title" : "Skyfall", "year" : 2012, "actor" : "Daniel Craig", "gross" : "$1,108,561,008" },
+  { "title" : "Thunderball", "year" : 1965, "actor" : "Sean Connery", "gross" : "$1,014,941,117" },
+  { "title" : "Goldfinger", "year" : 1964, "actor" : "Sean Connery", "gross" : "$912,257,512" },
+  { "title" : "Live and Let Die", "year" : 1973, "actor" : "Roger Moore", "gross" : "$825,110,761" },
+  { "title" : "You Only Live Twice", "year" : 1967, "actor" : "Sean Connery", "gross" : "$756,544,419" },
+  { "title" : "The Spy Who Loved Me", "year" : 1977, "actor" : "Roger Moore", "gross" : "$692,713,752" },
+  { "title" : "Casino Royale", "year" : 2006, "actor" : "Daniel Craig", "gross" : "$669,789,482" },
+  { "title" : "Moonraker", "year" : 1979, "actor" : "Roger Moore", "gross" : "$655,872,400" },
+  { "title" : "Diamonds Are Forever", "year" : 1971, "actor" : "Sean Connery", "gross" : "$648,514,469" },
+  { "title" : "Quantum of Solace", "year" : 2008, "actor" : "Daniel Craig", "gross" : "$622,246,378" },
+  { "title" : "From Russia with Love", "year" : 1963, "actor" : "Sean Connery", "gross" : "$576,277,964" },
+  { "title" : "Die Another Day", "year" : 2002, "actor" : "Pierce Brosnan", "gross" : "$543,639,638" },
+  { "title" : "Goldeneye", "year" : 1995, "actor" : "Pierce Brosnan", "gross" : "$529,548,711" },
+  { "title" : "On Her Majesty's Secret Service", "year" : 1969, "actor" : "George Lazenby", "gross" : "$505,899,782" },
+  { "title" : "The World is Not Enough", "year" : 1999, "actor" : "Pierce Brosnan", "gross" : "$491,617,153" },
+  { "title" : "For Your Eyes Only", "year" : 1981, "actor" : "Roger Moore", "gross" : "$486,468,881" },
+  { "title" : "Tomorrow Never Dies", "year" : 1997, "actor" : "Pierce Brosnan", "gross" : "$478,946,402" },
+  { "title" : "The Man with the Golden Gun", "year" : 1974, "actor" : "Roger Moore", "gross" : "$448,249,281" },
+  { "title" : "Dr. No", "year" : 1962, "actor" : "Sean Connery", "gross" : "$440,759,072" },
+  { "title" : "Octopussy", "year" : 1983, "actor" : "Roger Moore", "gross" : "$426,244,352" },
+  { "title" : "The Living Daylights", "year" : 1987, "actor" : "Timothy Dalton", "gross" : "$381,088,866" },
+  { "title" : "A View to a Kill", "year" : 1985, "actor" : "Roger Moore", "gross" : "$321,172,633" },
+  { "title" : "License to Kill", "year" : 1989, "actor" : "Timothy Dalton", "gross" : "$285,157,191" }
+];
+
+
+// Create a new array called bondTitles with only the titles of the Bond films, 
+// and console.log the new array.
+const findTheTitles = () => {
+	let bondTitles = [];
+for (let key in bondFilms) {
+	bondTitles.push(bondFilms[key]["title"]);
+} return bondTitles
+}
+console.log(findTheTitles());
+
+// Create a new array oddBonds, of only the Bond films released on odd-numbered years.
+
+const findTheOdds = () => {
+	let oddYearFilms = [];
+	for (let key in bondFilms){
+		if (bondFilms[key]["year"] % 2 !== 0){
+			oddYearFilms.push(bondFilms[key]["year"]);
+		}
+	}
+	return oddYearFilms
+}
+
+console.log(findTheOdds());
+
+// Determine the total cumulative gross of the Bond franchise, and console.log the result.
+// Hint: To make the grosses into usable numbers, look into the .replace Javascript method 
+// (there are many ways to do this, however). Look into parseInt() also.
+
+const makeCleanNumString = () => {
+	let grossString = ""
+	let buffer = "";
+	let bufferTwo = "";
+	let grossCleanArray =[];
+	let garbageArray = [];
+	let sum = 0;
+	for (let key in bondFilms){
+		grossString += (bondFilms[key]["gross"]);
+		grossString += " ";
+	} 
+	for (let i = 0; i <grossString.length; i++){
+		 if (grossString[i] === "$" || grossString[i] === ","){
+		 	garbageArray.push(grossString[i])
+		 } else { buffer += grossString[i];
+	}
+	}	for (let i = 0; i < buffer.length; i++){
+		bufferTwo += buffer[i];
+			if (buffer[i] === " "){
+			let output = parseInt(bufferTwo);
+			grossCleanArray.push(output);
+			bufferTwo = "";	
+	} 
+	} 	if (bufferTwo.length > 0) {
+		 grossCleanArray.push(bufferTwo);
+	}	for (let i = 0; i <grossCleanArray.length; i++){
+		sum += grossCleanArray[i]
+	}	return `bondFilms gross is $${sum}`
+}
+ console.log(makeCleanNumString());
+
+
+
+
+
+
+
+
+
+
+
 
 
