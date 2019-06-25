774cc0d6e4a041799ff274d59a9703393a2f0391
GA Denver <gadenver@Saras-MacBook-Air.local>
2019-06-24 18:10:56 -0600

    object-ception

--- js/app.js
+++ js/app.js
@@ -132,7 +132,36 @@ console.log(crazyObject.larry.nicknames[1]);
 console.log(crazyObject.larry.characters[1]);
 
 
+//With the following object:
 
+const inception = {
+   reality: {
+       dreamLayer1: {
+           dreamLayer2: {
+               dreamLayer3: {
+                   dreamLayer4: {
+                       dreamLayer5: {
+                           dreamLayer6: {
+                               limbo: "Joseph Gordon Levitt"
+                          }
+                       }
+                   }
+               }
+           }
+       }
+   }
+}
+//Change the value of limbo to null.
+
+
+// for(let key in inception){
+// 	if(inception[key] === "limbo")
+// 		inception[key].limbo = "null"
+// }
+// console.log()
+
+inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = "null"
+console.log(inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo)
 
 
 
